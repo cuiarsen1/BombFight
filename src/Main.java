93c0ac30feb6905536c0d4bf4eb2da44496fc776
Cui <ArsenYiJing.Cui@student.tdsb.on.ca>
2020-01-13 10:04:53 -0500

    Handle player movement

--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,14 @@ public class Main extends Application {
 	
 	AnimationTimer Timer;
 	
+	// booleans used to track which players need to be moved
+	private boolean moveBoolean1 = false;
+	private boolean moveBoolean2 = false;
+	
+	// Integers used to track the direction each player needs to be moved in
+	private int moveDirection1;
+	private int moveDirection2;
+	
 	/*Method used to create a StackPane of any object other than the Player 
 	(Space, Crate, BombMore, BombBoost)*/
 	public StackPane createImage(String imageFile) {
@@ -118,6 +126,17 @@ public class Main extends Application {
 		}
 	}
 	
+	public void createSpace(int x, int y) {
+		
+		StackPane spaceImage = createImage("file:Space.png");
+		
+		HBox row = (HBox)root.getChildren().get(y);
+    	row.getChildren().set(x, spaceImage);
+    	
+		Pair pair = new Pair(0, null);
+		map.mapArray[x][y] = pair;
+	}
+	
 	// Creates and initializes the player on the field
 	public Player createPlayer(int playerNum, int x, int y) {
 		
@@ -154,7 +173,17 @@ public class Main extends Application {
 	public Bomb createBomb(Player player) {
 		
 		Bomb bomb = new Bomb(0, 0);
-		StackPane bombImage = bomb.createImage("file:Bomb.png", "file:PlayerRed.png");
+		StackPane bombImage = null;
+		
+		if (player.playerNum == 1)
+		{
+			bombImage = bomb.createImage("file:Bomb.png", "file:PlayerRed.png");
+		}
+		
+		else if (player.playerNum == 2)
+		{
+			bombImage = bomb.createImage("file:Bomb.png", "file:PlayerBlue.png");
+		}
 		
 		HBox row = (HBox)root.getChildren().get(player.getY());
     	row.getChildren().set(player.getX(), bombImage);
@@ -163,6 +192,91 @@ public class Main extends Application {
 		map.mapArray[player.getX()][player.getY()] = pair;
 		
 		return bomb;
+	}
+	
+	// Method used to move players. The integer direction is represented with: 
+	// 1: Up 2: Down 3: Right 4: Left
+	public void movePlayers(int playerNum, int direction, String playerFile) {
+		
+		if (playerList.isEmpty())
+			return;
+		
+		if (direction == 1)
+		{
+			int tempY = playerList.get(playerNum - 1).getY();
+			playerList.get(playerNum - 1).setY(tempY - 1);
+			
+			StackPane playerView = playerList.get(playerNum - 1).createImage(playerFile);
+			
+			HBox row = (HBox)root.getChildren().get(playerList.get(playerNum - 1).getY());
+	    	row.getChildren().set(playerList.get(playerNum - 1).getX(), playerView);
+	    	
+	    	createSpace(playerList.get(playerNum - 1).getX(), tempY);
+	    	
+			Pair pair = new Pair(2, playerList.get(playerNum - 1));
+			map.mapArray[playerList.get(playerNum - 1).getX()][playerList.get(playerNum - 1).getY()] = pair;
+			
+			Pair pair2 = new Pair(0, null);
+			map.mapArray[playerList.get(playerNum - 1).getX()][tempY] = pair2;
+		}
+		
+		else if (direction == 2)
+		{
+			int tempY = playerList.get(playerNum - 1).getY();
+			playerList.get(playerNum - 1).setY(tempY + 1);
+			
+			StackPane playerView = playerList.get(playerNum - 1).createImage(playerFile);
+			
+			HBox row = (HBox)root.getChildren().get(playerList.get(playerNum - 1).getY());
+	    	row.getChildren().set(playerList.get(playerNum - 1).getX(), playerView);
+	    	
+	    	createSpace(playerList.get(playerNum - 1).getX(), tempY);
+	    	
+			Pair pair = new Pair(2, playerList.get(playerNum - 1));
+			map.mapArray[playerList.get(playerNum - 1).getX()][playerList.get(playerNum - 1).getY()] = pair;
+			
+			Pair pair2 = new Pair(0, null);
+			map.mapArray[playerList.get(playerNum - 1).getX()][tempY] = pair2;
+		}
+
+		else if (direction == 3)
+		{
+			int tempX = playerList.get(playerNum - 1).getX();
+			playerList.get(playerNum - 1).setX(tempX + 1);
+			
+			StackPane playerView = playerList.get(playerNum - 1).createImage(playerFile);
+			
+			HBox row = (HBox)root.getChildren().get(playerList.get(playerNum - 1).getY());
+	    	row.getChildren().set(playerList.get(playerNum - 1).getX(), playerView);
+	    	
+	    	createSpace(tempX, playerList.get(playerNum - 1).getY());
+	    	
+			Pair pair = new Pair(2, playerList.get(playerNum - 1));
+			map.mapArray[playerList.get(playerNum - 1).getX()][playerList.get(playerNum - 1).getY()] = pair;
+			
+			Pair pair2 = new Pair(0, null);
+			map.mapArray[tempX][playerList.get(playerNum - 1).getY()] = pair2;
+		}
+		
+		else if (direction == 4)
+		{
+			int tempX = playerList.get(playerNum - 1).getX();
+			playerList.get(playerNum - 1).setX(tempX - 1);
+			
+			StackPane playerView = playerList.get(playerNum - 1).createImage(playerFile);
+			
+			HBox row = (HBox)root.getChildren().get(playerList.get(playerNum - 1).getY());
+	    	row.getChildren().set(playerList.get(playerNum - 1).getX(), playerView);
+	    	
+	    	createSpace(tempX, playerList.get(playerNum - 1).getY());
+	    	
+			Pair pair = new Pair(2, playerList.get(playerNum - 1));
+			map.mapArray[playerList.get(playerNum - 1).getX()][playerList.get(playerNum - 1).getY()] = pair;
+			
+			Pair pair2 = new Pair(0, null);
+			map.mapArray[tempX][playerList.get(playerNum - 1).getY()] = pair2;
+		}
+		
 	}
 	
 	@Override
@@ -181,7 +295,7 @@ public class Main extends Application {
 		player1 = createPlayer(1, 0, 6);
 		player2 = createPlayer(2, 11, 5);
 		
-		Bomb bomb = createBomb(player1);
+		//Bomb bomb = createBomb(player1);
 		
 		Scene scene = new Scene(root, 600, 600);
 		
@@ -193,75 +307,120 @@ public class Main extends Application {
 				if (playerList.isEmpty())
 					return;
 
+				// Handles movement for Player 1
+				
 				if (event.getCode() == KeyCode.UP)
 				{
-					int tempY = player1.getY();
-					player1.setY(tempY - 1);
-					
-					StackPane playerView = player1.createImage("file:PlayerRed.png");
-					
-					HBox row = (HBox)root.getChildren().get(player1.getY());
-			    	row.getChildren().set(player1.getX(), playerView);
-			    	
-					Pair pair = new Pair(2, player1);
-					map.mapArray[player1.getX()][player1.getY()] = pair;
-					
-					Pair pair2 = new Pair(0, null);
-					map.mapArray[player1.getX()][tempY] = pair2;
+					moveBoolean1 = true;
+					moveDirection1 = 1;
 				}
 				
 				else if (event.getCode() == KeyCode.DOWN)
 				{
-					int tempY = player1.getY();
-					player1.setY(tempY + 1);
-					
-					StackPane playerView = player1.createImage("file:PlayerRed.png");
-					
-					HBox row = (HBox)root.getChildren().get(player1.getY());
-			    	row.getChildren().set(player1.getX(), playerView);
-			    	
-					Pair pair = new Pair(2, player1);
-					map.mapArray[player1.getX()][player1.getY()] = pair;
-					
-					Pair pair2 = new Pair(0, null);
-					map.mapArray[player1.getX()][tempY] = pair2;
+					moveBoolean1 = true;
+					moveDirection1 = 2;
 				}
 					
 				else if (event.getCode() == KeyCode.RIGHT)
 				{
-					int tempX = player1.getX();
-					player1.setX(tempX + 1);
-					
-					StackPane playerView = player1.createImage("file:PlayerRed.png");
-					
-					HBox row = (HBox)root.getChildren().get(player1.getY());
-			    	row.getChildren().set(player1.getX(), playerView);
-			    	
-					Pair pair = new Pair(2, player1);
-					map.mapArray[player1.getX()][player1.getY()] = pair;
-					
-					Pair pair2 = new Pair(0, null);
-					map.mapArray[tempX][player1.getY()] = pair2;
+					moveBoolean1 = true;
+					moveDirection1 = 3;
 				}
 				
 				else if (event.getCode() == KeyCode.LEFT)
 				{
-					int tempX = player1.getX();
-					player1.setX(tempX - 1);
-					
-					StackPane playerView = player1.createImage("file:PlayerRed.png");
-					
-					HBox row = (HBox)root.getChildren().get(player1.getY());
-			    	row.getChildren().set(player1.getX(), playerView);
-			    	
-					Pair pair = new Pair(2, player1);
-					map.mapArray[player1.getX()][player1.getY()] = pair;
-					
-					Pair pair2 = new Pair(0, null);
-					map.mapArray[tempX][player1.getY()] = pair2;
+					moveBoolean1 = true;
+					moveDirection1 = 4;
 				}
 			}
 		});
+		
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+
+				// If there is no player on the screen, don't do anything
+				if (playerList.isEmpty())
+					return;
+				
+				// Handles movement for Player 2
+				
+				if (event.getCode() == KeyCode.W)
+				{
+					moveBoolean2 = true;
+					moveDirection2 = 1;
+				}
+				
+				else if (event.getCode() == KeyCode.S)
+				{
+					moveBoolean2 = true;
+					moveDirection2 = 2;
+				}
+					
+				else if (event.getCode() == KeyCode.D)
+				{
+					moveBoolean2 = true;
+					moveDirection2 = 3;
+				}
+				
+				else if (event.getCode() == KeyCode.A)
+				{
+					moveBoolean2 = true;
+					moveDirection2 = 4;
+				}
+			}
+		});
+		
+		scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+
+				// If there is no player on the screen, don't do anything
+				if (playerList.isEmpty())
+					return;
+
+				// Handles stopping movement for Player 1
+				
+				if (event.getCode() == KeyCode.UP)
+					moveBoolean1 = false;
+
+				else if (event.getCode() == KeyCode.DOWN)
+					moveBoolean1 = false;
+
+				else if (event.getCode() == KeyCode.RIGHT)
+					moveBoolean1 = false;
+
+				else if (event.getCode() == KeyCode.LEFT)
+					moveBoolean1 = false;
+
+			}
+		});
+		
+		scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+
+				// If there is no player on the screen, don't do anything
+				if (playerList.isEmpty())
+					return;
+
+				// Handles stopping movement for Player 2
+				
+				if (event.getCode() == KeyCode.W)
+					moveBoolean2 = false;
+
+				else if (event.getCode() == KeyCode.S)
+					moveBoolean2 = false;
+
+				else if (event.getCode() == KeyCode.D)
+					moveBoolean2 = false;
+
+				else if (event.getCode() == KeyCode.A)
+					moveBoolean2 = false;
+
+			}
+		});
+
 		
 		// Animation timer used to animate the game
 		Timer = new AnimationTimer() {
@@ -269,32 +428,51 @@ public class Main extends Application {
 			// Variables used to track the time
 			
 			long oldTimeVelocity = 0;
-			long intervalVelocity = 500000000l;
+			long intervalVelocity = 300000000;
 
 			@Override
 			public void handle(long time) {
 				
-				// Animates all the objects every frame CHANGE THIS OBVIOUSLY
-				try {
-					upDateMove();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
 				oldTimeVelocity += 1;
 
 				// Move the player every half a second if direction key held down
 				if (time - oldTimeVelocity > intervalVelocity) {
-					upDateVelocity();
+					try {
+						upDateMove();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 					oldTimeVelocity = time;
 				}
 
 			}
 		};
+		
+		Timer.start();
 
 		primaryStage.setTitle("Bomb Fight");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	private void upDateMove() throws IOException {
+		
+		if (moveBoolean1 == true)
+		{
+			movePlayers(1, moveDirection1, "file:PlayerRed.png");
+		}
+		
+		if (moveBoolean2 == true)
+		{
+			movePlayers(2, moveDirection2, "file:PlayerBlue.png");
+		}
+
+
+		/*// If the player collides with an obstacle, end the game
+		if (stopGame == true)
+			gameOver();*/
+
 	}
 
 	public static void main(String[] args) throws IOException {
